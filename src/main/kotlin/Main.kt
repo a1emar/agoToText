04b372352fe873c1ca0cad1bf643f4654ad21df4
AlexeYeS <m0use@mail.ru>
2022-08-14 01:48:36 +0300

    add fun minuts

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,23 @@ fun main() {
     println(name + " был(а) " + agoToText(goOnline))
 }
 
-fun agoToText(goOnline:Int) {
-    when (goOnline) {
+fun agoToText(goOnline:Int): String {
+    val stringLine = when (goOnline) {
         in 0..60 -> "только что"
+        in 61..60*60 -> minuts(goOnline)
+        in 60*60+1..24*60*60 -> "x часов назад"
+        in 24*60*60+1..48*60*60 -> "сегодня"
+        in 48*60*60+1..72*60*60 -> "вчера"
+        else -> "давно"
     }
+    return stringLine
+}
+
+fun minuts(goOnline: Int): String {
+    val minuts = when (goOnline) {
+        in 61..120 -> "gg"//21*60+1..1320,
+
+        else -> "jj"
+    }
+    return minuts
 }
